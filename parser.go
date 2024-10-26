fd58d9a9e077fad2a3a0b4aecd8073508bafccc0
Eric Anderson <anderson.eric.t@gmail.com>
2024-10-26 18:09:33 -0400

    restructuring reference output

--- parser.go
+++ parser.go
@@ -10,74 +10,90 @@ import (
 	"strings"
 )
 
-// FileParser struct
 type FileParser struct {
-	docMap map[string]FileData // Map to hold file names and their corresponding doc comments and function details
+	docMap map[string]FileData
 }
 
-// FileData represents the structure of the data stored for each file
 type FileData struct {
-	Docs      []string         `json:"docs"`
+	Package   string           `json:"package"`
+	Imports   []string         `json:"imports"`
 	Functions []FunctionDetail `json:"functions"`
+	Variables []VariableDetail `json:"variables"`
 }
 
-// FunctionDetail represents the structure of function details with associated documentation
 type FunctionDetail struct {
 	Name        string   `json:"name"`
-	Docs        []string `json:"docs"` // Documentation comments for the function
+	Docs        []string `json:"docs"`
 	Params      []string `json:"params"`
 	ParamTypes  []string `json:"param_types"`
 	ReturnTypes []string `json:"return_types"`
 }
 
-// NewFileParser initializes a new FileParser
+type VariableDetail struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+	Docs string `json:"docs"`
+}
+
 func NewFileParser() *FileParser {
 	return &FileParser{docMap: make(map[string]FileData)}
 }
 
-// ParseFile parses the Go file and extracts documentation comments and function details
 func (fp *FileParser) ParseFile(filePath string) {
 	fset := token.NewFileSet()
 	node, err := parser.ParseFile(fset, filePath, nil, parser.ParseComments)
 	if err != nil {
-		log.Printf("Error parsing file: %v", err)
+		log.Printf("Error parsing file '%s': %v", filePath, err)
 		return
 	}
 
-	fileData := FileData{}
+	fileData := FileData{
+		Package: node.Name.Name,
+	}
 
-	// Iterate through declarations in the file
+	for _, imp := range node.Imports {
+		if imp.Path != nil {
+			fileData.Imports = append(fileData.Imports, strings.Trim(imp.Path.Value, `"`))
+		}
+	}
+
 	for _, decl := range node.Decls {
 		switch d := decl.(type) {
 		case *ast.GenDecl:
-			// Handle package comments
-			if doc := d.Doc; doc != nil {
-				fileData.Docs = append(fileData.Docs, strings.TrimSpace(doc.Text()))
+			if d.Tok == token.VAR || d.Tok == token.CONST {
+				for _, spec := range d.Specs {
+					if vs, ok := spec.(*ast.ValueSpec); ok && len(vs.Names) > 0 {
+						varDetail := VariableDetail{
+							Name: vs.Names[0].Name,
+							Type: formatType(vs.Type),
+						}
+						if vs.Doc != nil {
+							varDetail.Docs = sanitizeDoc(vs.Doc.Text())
+						} else if vs.Comment != nil {
+							varDetail.Docs = sanitizeDoc(vs.Comment.Text())
+						}
+						fileData.Variables = append(fileData.Variables, varDetail)
+					}
+				}
 			}
 		case *ast.FuncDecl:
-			// Prepare function detail
 			funcDetail := FunctionDetail{
 				Name: d.Name.Name,
 			}
 
-			// Extract documentation comments for the function
 			if d.Doc != nil {
-				funcDetail.Docs = append(funcDetail.Docs, strings.TrimSpace(d.Doc.Text()))
+				funcDetail.Docs = append(funcDetail.Docs, sanitizeDoc(d.Doc.Text()))
 			}
 
-			// Extract parameters
 			if d.Type.Params != nil {
 				for _, param := range d.Type.Params.List {
-					// Add parameter names
 					for _, name := range param.Names {
 						funcDetail.Params = append(funcDetail.Params, name.Name)
 					}
-					// Add parameter types
 					funcDetail.ParamTypes = append(funcDetail.ParamTypes, formatType(param.Type))
 				}
 			}
 
-			// Extract return types
 			if d.Type.Results != nil {
 				for _, result := range d.Type.Results.List {
 					funcDetail.ReturnTypes = append(funcDetail.ReturnTypes, formatType(result.Type))
@@ -88,32 +104,33 @@ func (fp *FileParser) ParseFile(filePath string) {
 		}
 	}
 
-	// Store the parsed data for the file
 	fp.docMap[filePath] = fileData
 
-	// Write the JSON to the reference.json file
 	if err := fp.writeJSONToFile("reference.json"); err != nil {
 		log.Printf("Error writing JSON to file: %v", err)
 	}
 }
 
-// writeJSONToFile writes the documentation map to a JSON file
 func (fp *FileParser) writeJSONToFile(filename string) error {
 	file, err := os.Create(filename)
 	if err != nil {
-		return err // Return the error to handle it in ParseFile
+		return err
 	}
 	defer file.Close()
 
 	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ") // Set indentation for the JSON output
+	encoder.SetIndent("", "  ")
+
 	if err := encoder.Encode(fp.docMap); err != nil {
-		return err // Return the error to handle it in ParseFile
+		return err
 	}
 	return nil
 }
 
-// formatType converts an ast.Expr to a string representation
+func sanitizeDoc(doc string) string {
+	return strings.TrimSpace(strings.ReplaceAll(doc, "\n", " "))
+}
+
 func formatType(expr ast.Expr) string {
 	if expr == nil {
 		return ""
@@ -123,32 +140,35 @@ func formatType(expr ast.Expr) string {
 	case *ast.Ident:
 		return t.Name
 	case *ast.SelectorExpr:
-		return t.X.(*ast.Ident).Name + "." + t.Sel.Name
+		if x, ok := t.X.(*ast.Ident); ok {
+			return x.Name + "." + t.Sel.Name
+		}
+		return t.Sel.Name
 	case *ast.ArrayType:
-		return "[]" + formatType(t.Elt) // handle array type
+		return "[]" + formatType(t.Elt)
 	case *ast.MapType:
-		return "map[" + formatType(t.Key) + "]" + formatType(t.Value) // handle map type
+		return "map[" + formatType(t.Key) + "]" + formatType(t.Value)
 	case *ast.StarExpr:
-		return "*" + formatType(t.X) // handle pointer type
+		return "*" + formatType(t.X)
 	case *ast.FuncType:
-		return "func" + formatFuncType(t) // handle function type
+		return "func" + formatFuncType(t)
 	default:
-		return "<unknown type>" // Provide a fallback for unknown types
+		return "<unknown type>"
 	}
 }
 
-// formatFuncType formats the function type
 func formatFuncType(funcType *ast.FuncType) string {
-	paramTypes := ""
-	if funcType.Params != nil {
-		paramTypes += "("
-		for i, param := range funcType.Params.List {
-			paramTypes += formatType(param.Type)
-			if i < len(funcType.Params.List)-1 {
-				paramTypes += ", "
-			}
-		}
-		paramTypes += ")"
+	if funcType == nil {
+		return ""
 	}
+
+	paramTypes := "("
+	for i, param := range funcType.Params.List {
+		paramTypes += formatType(param.Type)
+		if i < len(funcType.Params.List)-1 {
+			paramTypes += ", "
+		}
+	}
+	paramTypes += ")"
 	return paramTypes
 }
